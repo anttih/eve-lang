5422f7529bdb4bd5e757422a4ce24a41ae570fe3
Antti Holvikari <anttih@gmail.com>
2014-10-17 22:35:03 +0300

    Rename Parser to Reader

--- Reader.hs
+++ Reader.hs
@@ -1,4 +1,4 @@
-module Parser (
+module Reader (
   Result(Ok, Fail),
   readLispData
   ) where
@@ -26,123 +26,123 @@ instance Applicative Result where
   pure = return
   (<*>) = ap
 
-data Parser a = Parser { parse :: String -> Result a }
+data Reader a = Reader { runReader :: String -> Result a }
 
-instance Monad Parser where
-  (Parser p) >>= f = Parser p2 where
+instance Monad Reader where
+  (Reader p) >>= f = Reader p2 where
              p2 cs = case p cs
                      of Fail -> Fail
-                        (Ok a rs) -> parse (f a) rs
-  return v = Parser (Ok v)
+                        (Ok a rs) -> runReader (f a) rs
+  return v = Reader (Ok v)
 
-instance Functor Parser where
-  fmap f (Parser p) = Parser newP where
+instance Functor Reader where
+  fmap f (Reader p) = Reader newP where
             newP cs = case p cs of
                         (Ok r s) -> Ok (f r) s
                         _ -> Fail
 
 
-instance Applicative Parser where
+instance Applicative Reader where
   pure = return
   (<*>) = ap
 
-both :: Parser a -> Parser a -> Parser a
-both p1 p2 = Parser p where
-      p cs = case parse p1 cs
+both :: Reader a -> Reader a -> Reader a
+both p1 p2 = Reader p where
+      p cs = case runReader p1 cs
              of Fail -> Fail
-                (Ok _ _) -> parse p2 cs
+                (Ok _ _) -> runReader p2 cs
 
-either :: Parser a -> Parser a -> Parser a
-either p1 p2 = Parser p where
-         p s = case parse p1 s
-               of Fail -> parse p2 s
+either :: Reader a -> Reader a -> Reader a
+either p1 p2 = Reader p where
+         p s = case runReader p1 s
+               of Fail -> runReader p2 s
                   ok -> ok
 
-seq :: Parser a -> Parser b -> Parser (a, b)
-seq p1 p2 = Parser p where
-      p s = case parse p1 s of
-              (Ok c1 rs) -> case parse p2 rs of
+seq :: Reader a -> Reader b -> Reader (a, b)
+seq p1 p2 = Reader p where
+      p s = case runReader p1 s of
+              (Ok c1 rs) -> case runReader p2 rs of
                              (Ok c2 rs2) -> Ok (c1, c2) rs2
                              Fail -> Fail
               Fail -> Fail
 
-zeroMany :: Parser a -> Parser [a]
-zeroMany p = Parser pa where
+zeroMany :: Reader a -> Reader [a]
+zeroMany p = Reader pa where
   pa = loop []
-  loop acc cs = case parse p cs of
+  loop acc cs = case runReader p cs of
                  Fail -> Ok (reverse acc) cs
                  (Ok c rest) -> loop (c:acc) rest
 
-oneMany :: Parser a -> Parser [a]
+oneMany :: Reader a -> Reader [a]
 oneMany p = do
     x <- p
     xs <- zeroMany p
     return (x:xs)
 
-notEmpty :: Parser Char
-notEmpty = Parser p where
+notEmpty :: Reader Char
+notEmpty = Reader p where
      p s = case s
            of [] -> Fail
               (fs:rest) -> Ok fs rest
 
-charTest :: (Char -> Bool) -> Parser Char
-charTest f = both notEmpty (Parser p) where
+charTest :: (Char -> Bool) -> Reader Char
+charTest f = both notEmpty (Reader p) where
        p s = if not $ f $ head s
              then Fail
              else Ok (head s) (tail s)
 
-numeric ::  Parser Char
+numeric ::  Reader Char
 numeric = charTest isDigit
 
-alpha ::  Parser Char
+alpha ::  Reader Char
 alpha = charTest isAlpha
 
-alphaNumeric ::  Parser Char
+alphaNumeric ::  Reader Char
 alphaNumeric = charTest isAlphaNum
 
-space ::  Parser Char
+space ::  Reader Char
 space = charTest isSpace
 
-whitespace ::  Parser String
+whitespace ::  Reader String
 whitespace = zeroMany space
 
-char ::  Char -> Parser Char
+char ::  Char -> Reader Char
 char c = charTest (== c)
 
-notChar ::  Char -> Parser Char
+notChar ::  Char -> Reader Char
 notChar c = charTest $ not . (== c)
 
-token ::  Parser b -> Parser b
+token ::  Reader b -> Reader b
 token p = snd <$> seq whitespace p
 
-symbolSpecial ::  Parser Char
+symbolSpecial ::  Reader Char
 symbolSpecial = charTest $ \c -> c `elem` "+-/=><*!?"
 
-symbolSeq ::  Parser String
+symbolSeq ::  Reader String
 symbolSeq = do first <- either alpha symbolSpecial
                rest <- zeroMany alphaNumeric
                return (first:rest)
 
-symbol ::  Parser LispData
+symbol ::  Reader LispData
 symbol = Symbol <$> token symbolSeq
 
-identifier ::  String -> Parser LispData
+identifier ::  String -> Reader LispData
 identifier name = do (Symbol sym) <- symbol
-                     Parser $ equals sym where
+                     Reader $ equals sym where
   equals sym cs = if sym == name
                   then Ok (Symbol sym) cs
                   else Fail
 
-boolean ::  Parser LispData
+boolean ::  Reader LispData
 boolean = either (fmap (const $ LispBool True) (identifier "true"))
                  (fmap (const $ LispBool False) (identifier "false"))
 
-keyword ::  Parser LispData
+keyword ::  Reader LispData
 keyword = do _ <- token $ char ':'
              str <- symbolSeq
              return (Keyword str)
 
-string ::  Parser LispData
+string ::  Reader LispData
 string = do _ <- token quote
             xs <- zeroMany stringChar
             _ <- quote
@@ -150,24 +150,24 @@ string = do _ <- token quote
   quote = char '"'
   stringChar = notChar '"'
 
-number :: Parser LispData
+number :: Reader LispData
 number = Number . read <$> token (oneMany numeric)
 
-list ::  Parser LispData
+list ::  Reader LispData
 list = do _ <- token $ char '('
           xs <- zeroMany expr
           _ <- token $ char ')'
           return $ Sexpr (foldr cons Null xs)
 
-lispMap ::  Parser LispData
+lispMap ::  Reader LispData
 lispMap = do _ <- token $ char '{'
              xs <- zeroMany (seq keyword expr)
              _ <- token $ char '}'
              return $ LispMap (M.fromList xs)
 
-expr ::  Parser LispData
+expr ::  Reader LispData
 expr =  boolean `either` string `either` number `either` symbol `either` keyword  `either` list `either` lispMap
 
 -- @todo implement instance Read
 readLispData :: String -> Result LispData
-readLispData = parse expr
+readLispData = runReader expr

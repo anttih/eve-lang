3728c50da389a2767cd4f23c3f3b16e36a04adc3
Antti Holvikari <anttih@gmail.com>
2014-10-17 23:06:07 +0300

    Rename Syntax to Parser

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,4 @@
-module Syntax where
+module Parser where
 
 import Prelude hiding (concat, sequence)
 import Reader
@@ -23,34 +23,34 @@ data Ast = Let [String] [Ast] Ast
          | Alternative Ast Ast Ast
          | Seq [Ast] deriving (Show)
 
-newtype Checker a = Checker { runChecker :: List LispData -> Syntax a }
+newtype Parser a = Parser { runParser :: List LispData -> Syntax a }
 
-instance Monad Checker where
-  checker >>= f = Checker bind where
+instance Monad Parser where
+  checker >>= f = Parser bind where
     bind xs =
-      let syntax = runChecker checker xs in
+      let syntax = runParser checker xs in
       EitherT $ do
         a <- runEitherT syntax
         case a of
           Left e -> return $ Left e
-          Right (x, rest) -> runEitherT $ runChecker (f x) rest
-  return x = Checker (\xs -> return (x, xs))
+          Right (x, rest) -> runEitherT $ runParser (f x) rest
+  return x = Parser (\xs -> return (x, xs))
 
-instance Applicative Checker where
+instance Applicative Parser where
   pure = return
   (<*>) = ap
 
-instance Functor Checker where
-  fmap f checker = Checker c where
+instance Functor Parser where
+  fmap f checker = Parser c where
     c xs =
-      let syntax = runChecker checker xs in
+      let syntax = runParser checker xs in
       EitherT $ do
         a <- runEitherT syntax
         case a of
           Left e -> return $ Left e
           Right (x, rest) -> return $ Right (f x, rest)
 
-lispLet :: Checker Ast
+lispLet :: Parser Ast
 lispLet = sexpr $ do
   void $ symbol "let"
   b <- sexpr bindings
@@ -59,30 +59,30 @@ lispLet = sexpr $ do
   popFrame
   return (Let (fst <$> b) (snd <$> b) (Seq body))
 
-bindings :: Checker [(String, Ast)]
+bindings :: Parser [(String, Ast)]
 bindings = zeroMany $ sexpr $ (,) <$> binding <*> lispExpr where
   binding = (\(Symbol s) -> s) <$> anySymbol
 
-addBinding :: String -> Checker ()
-addBinding name = Checker c where
+addBinding :: String -> Parser ()
+addBinding name = Parser c where
   c xs = state (\s -> (((), xs), newState name s)) where
     newState x [] = [[x]]
     newState x (frame:prev) = (x : frame) : prev
 
-pushFrame :: [String] -> Checker ()
-pushFrame names = Checker c where
+pushFrame :: [String] -> Parser ()
+pushFrame names = Parser c where
   c xs = state (\prev -> (((), xs), names : prev))
 
-popFrame :: Checker ()
-popFrame = Checker c where
+popFrame :: Parser ()
+popFrame = Parser c where
   c xs = state (\s -> (((), xs), pop s)) where
     pop [] = []
     pop (_:rest) = rest
 
-sequence :: Checker [Ast]
+sequence :: Parser [Ast]
 sequence = zeroMany lispExpr
 
-definition :: Checker Ast
+definition :: Parser Ast
 definition = sexpr $ do
   void $ symbol "def"
   (Symbol name) <- anySymbol
@@ -90,7 +90,7 @@ definition = sexpr $ do
   addBinding name
   return $ Definition name expr
 
-funcDefinition :: Checker Ast
+funcDefinition :: Parser Ast
 funcDefinition = sexpr $ do
   void $ symbol "defn"
   (Symbol name) <- anySymbol
@@ -101,94 +101,94 @@ funcDefinition = sexpr $ do
   popFrame
   return $ Definition name (Function params (Seq impl))
 
-doBlock :: Checker Ast
+doBlock :: Parser Ast
 doBlock = sexpr $ do
   void $ symbol "do"
   xs <- sequence
   return $ Seq xs
 
-reference :: Checker Ast
+reference :: Parser Ast
 reference = do
   (Symbol name) <- anySymbol
   addRef name where
-    addRef :: String -> Checker Ast
-    addRef name = Checker f where
+    addRef :: String -> Parser Ast
+    addRef name = Parser f where
       f xs = do
         s <- get
         return $ maybe (FreeReference name, xs) (const (LocalReference name, xs)) $ find (elem name) s
 
 -- @todo Won't work? State from c1 is being used in c2
-(<&>) :: Checker a -> Checker b -> Checker b
-(<&>) c1 c2 = Checker f where
+(<&>) :: Parser a -> Parser b -> Parser b
+(<&>) c1 c2 = Parser f where
   f xs =
-    let syntax = runChecker c1 xs in
+    let syntax = runParser c1 xs in
     EitherT $ do
       a <- runEitherT syntax
       case a of
         Left e -> return $ Left e
-        Right _ -> runEitherT $ runChecker c2 xs
+        Right _ -> runEitherT $ runParser c2 xs
 
 -- the 'or' operator
-(<|>) :: Checker a -> Checker a -> Checker a
-(<|>) c1 c2 = Checker f where
+(<|>) :: Parser a -> Parser a -> Parser a
+(<|>) c1 c2 = Parser f where
   f xs =
-    let syntax = runChecker c1 xs in
+    let syntax = runParser c1 xs in
     EitherT $ do
       a <- runEitherT syntax
       case a of
-        Left _ -> runEitherT $ runChecker c2 xs
+        Left _ -> runEitherT $ runParser c2 xs
         Right x -> return $ Right x
 
-anyVal :: Checker LispData
-anyVal = Checker f where
+anyVal :: Parser LispData
+anyVal = Parser f where
   f Null = left "Expecting a value, but got nothing"
   f (Pair x rest) = return (x, rest)
 
-endSexpr :: Checker ()
-endSexpr = Checker f where
+endSexpr :: Parser ()
+endSexpr = Parser f where
   f Null = return ((), Null)
   f (Pair x _) = left $ "Expecting the end of list, but got " ++ show x
 
 -- Parse values from inside a lisp list (...) with checker `c`. The checker
 -- should consume all of the values inside the list.
-sexpr :: Checker a -> Checker a
-sexpr c = anyVal <&> Checker isSexpr where
+sexpr :: Parser a -> Parser a
+sexpr c = anyVal <&> Parser isSexpr where
   isSexpr (Pair x rest) = case x of
-    (Sexpr xs) -> (\(res, _) -> (res, rest)) <$> runChecker (c <* endSexpr) xs
+    (Sexpr xs) -> (\(res, _) -> (res, rest)) <$> runParser (c <* endSexpr) xs
     _ -> left $ "Expecting a list, got " ++ show x
   isSexpr _ = left ""
 
-zeroMany :: Checker a -> Checker [a]
-zeroMany c = Checker p where
+zeroMany :: Parser a -> Parser [a]
+zeroMany c = Parser p where
   p l = EitherT $ loop [] l where
     loop acc Null = return $ Right (acc, Null)
     loop acc rest = do
-      a <- runEitherT (runChecker c rest)
+      a <- runEitherT (runParser c rest)
       case a of
         Left e -> return $ Left e
         Right (x, xs) -> loop (acc ++ [x]) xs
 
-check :: (LispData -> Bool) -> String -> Checker LispData
-check f msg = Checker checker where
+check :: (LispData -> Bool) -> String -> Parser LispData
+check f msg = Parser checker where
   checker (Pair x rest) = if f x then return (x, rest) else left $ msg ++ ", got " ++ show x
   checker _ = left "Fail. This should not happen."
 
-anySymbol ::  Checker LispData
+anySymbol ::  Parser LispData
 anySymbol = anyVal <&> check sym "Expecting any symbol" where
   sym (Symbol _) = True
   sym _ = False
 
-symbol ::  String -> Checker LispData
+symbol ::  String -> Parser LispData
 symbol name = anySymbol <&> check named ("Expecting symbol " ++ name) where
   named (Symbol sym) = sym == name
   named _ = False
 
-string :: Checker LispData
+string :: Parser LispData
 string = check s "Expecting a string" where
   s (Str _) = True
   s _ = False
 
-literal :: Checker Ast
+literal :: Parser Ast
 literal = Literal <$> check val "Expecting a literal" where
   val (LispBool _) = True
   val (Number _) = True
@@ -197,22 +197,22 @@ literal = Literal <$> check val "Expecting a literal" where
   val (Keyword _) = True
   val _ = False
 
-lispExpr :: Checker Ast
+lispExpr :: Parser Ast
 lispExpr = literal <|> definition <|> funcDefinition <|> reference <|> lispLet <|> doBlock
 
-parse :: Checker LispData -> String -> Either String LispData
+parse :: Parser LispData -> String -> Either String LispData
 parse c input = case readLispData input of
-  Ok e _ -> either Left (Right . fst) $ evalState (runEitherT (runChecker c (Pair e Null))) []
+  Ok e _ -> either Left (Right . fst) $ evalState (runEitherT (runParser c (Pair e Null))) []
   Fail -> Left "Parse error"
 
 parseExpr :: String -> Either String Ast
 parseExpr input = case readLispData input of
-  Ok e _ -> either Left (Right . fst) $ evalState (runEitherT (runChecker lispExpr (Pair e Null))) []
+  Ok e _ -> either Left (Right . fst) $ evalState (runEitherT (runParser lispExpr (Pair e Null))) []
   Fail -> Left "Parse error"
 
-runExprState' :: Checker Ast -> String -> Either String Bindings
+runExprState' :: Parser Ast -> String -> Either String Bindings
 runExprState' checker input = case readLispData input of
-  Ok e _ -> case runState (runEitherT (runChecker checker (Pair e Null))) [] of
+  Ok e _ -> case runState (runEitherT (runParser checker (Pair e Null))) [] of
     (Left m, _) -> Left m
     (Right _, s) -> Right s
   Fail -> Left "Parse error"

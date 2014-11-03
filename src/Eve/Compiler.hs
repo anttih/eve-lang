563e6724dedb97072a698a60eb5589108b84b81c
Antti Holvikari <anttih@gmail.com>
2014-11-03 07:40:45 +0200

    Stack frames are maps

--- src/Eve/Compiler.hs
+++ src/Eve/Compiler.hs
@@ -5,12 +5,14 @@ import Eve.Data
 import Eve.Parser
 
 data OpCodeF next = Refer String next
-                 | Constant LispData next
-                 | Assign String next
-                 | Close [String] (OpCodeF next) next
-                 | Def String next
-                 | Test (OpCodeF next) (OpCodeF next) next
-                 | Halt
+                  | Constant LispData next
+                  | Assign String next
+                  | Close [String] (OpCodeF next) next
+                  | Def String next
+                  | Test (OpCodeF next) (OpCodeF next) next
+                  | Halt deriving Show
+
+type OpCode a = Free OpCodeF a
 
 instance Functor OpCodeF where
   fmap f (Refer v next) = Refer v (f next)
@@ -21,9 +23,12 @@ instance Functor OpCodeF where
   fmap f (Test then' alt next) =  Test (fmap f then') (fmap f alt) (f next)
   fmap _ Halt = Halt
 
-compile' :: Free OpCodeF a -> Ast -> Free OpCodeF a
-compile' n (Literal v) = Free (Constant v n)
+compile' :: Ast -> OpCode a -> OpCode a
+compile' (Literal v) n = Free (Constant v n)
+compile' (LocalReference s) n = Free (Refer s n)
+compile' (Seq xs) n = foldr compile' n xs
+compile' (Definition name ast) n = compile' ast (Free $ Def name n)
 compile' _ _ = Free Halt
 
-compile :: Ast -> Free OpCodeF ()
-compile = compile' (Free Halt)
+compile :: Ast -> OpCode ()
+compile ast = compile' ast (Free Halt)
--- src/Eve/VM.hs
+++ src/Eve/VM.hs
@@ -5,17 +5,32 @@ module Eve.VM (
 import Control.Monad.Free
 import Control.Monad.State.Strict
 import Control.Exception.Base
+
+import Data.Maybe
+import Data.List
+import Data.Functor
+import qualified Data.Map.Strict as Map
+
 import Eve.Data
 import Eve.Compiler
 
-type VM = StateT (LispData, [[String]]) IO ()
+type VM = StateT (LispData, [Map.Map String LispData]) IO ()
 
 run :: Free OpCodeF a -> VM
 run (Free (Constant v n)) = modify (\(_, env) -> (v, env)) >> run n
-run (Free Halt)           = do
-  (a, _) <- get
-  liftIO (print a)
+run (Free (Refer v n))    = modify (\(_, env) -> (refer v env, env)) >> run n
+run (Free (Def name n))   = modify updateFrame >> run n where
+  updateFrame (v, [])       = (v, [Map.singleton name v])
+  updateFrame (v, frame:xs) = (v, Map.insert name v frame : xs)
+run (Free Halt)           = get >>= liftIO . print . fst
 run (Pure _)              = liftIO (throwIO (userError "Fail"))
+run _                     = liftIO (throwIO (userError "Not implemented"))
+
+-- This is unsafe, but using this for LocalReferences should always succeed
+refer :: String -> [Map.Map String LispData] -> LispData
+refer k env = fromJust $ join $ find local (Map.lookup k <$> env) where
+  local (Just _) = True
+  local Nothing = False
 
 exec :: Free OpCodeF () -> IO ()
 exec op = void $ runStateT (run op) (Sexpr Null, [])
